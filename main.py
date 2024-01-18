23dfb2502787ce4ed7a500325b8bc8703f8037f0
Silas Martin <sm@akamo.de>
2024-01-18 20:59:33 +0100

    feat: added support for WhatsApp forwarding

--- main.py
+++ main.py
@@ -33,14 +33,35 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def transcribe(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print("received voice message")
+    print("Received voice message")
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text="Received voice message, transcription running...",
     )
-    new_file = await context.bot.get_file(update.message.voice.file_id)
-    await new_file.download_to_drive(custom_path="./voice.ogg")
-    result = model.transcribe("voice.ogg")
+
+    # Get the voice message
+    voice_message = update.message.voice or update.message.audio
+
+    # Determine the file extension --> adds support for forwarding from WhatsApp
+    file_extension = ".ogg"  # Default to .ogg for voice messages
+    if update.message.voice:
+        file_extension = ".ogg"
+    elif update.message.audio:
+        if update.message.audio.mime_type:
+            mime_type = update.message.audio.mime_type
+            if mime_type == "audio/mp4":
+                file_extension = ".m4a"
+            elif mime_type == "audio/mpeg":
+                file_extension = ".mp3"
+
+    file_path = f"./voice{file_extension}"
+
+    # Download the file
+    new_file = await context.bot.get_file(voice_message.file_id)
+    await new_file.download_to_drive(custom_path=file_path)
+
+    # Transcribe the audio file
+    result = model.transcribe(file_path)
     print(result["text"])
     await context.bot.send_message(
         chat_id=update.effective_chat.id, text=result["text"]
@@ -48,18 +69,16 @@ async def transcribe(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 if __name__ == "__main__":
-    application = (
-        ApplicationBuilder()
-        .token(TOKEN)
-        .build()
-    )
+    application = ApplicationBuilder().token(TOKEN).build()
 
     start_handler = CommandHandler("start", start)
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     transcription_handler = MessageHandler(filters.VOICE, transcribe)
+    audio_handler = MessageHandler(filters.AUDIO, transcribe)
 
     application.add_handler(start_handler)
     application.add_handler(echo_handler)
     application.add_handler(transcription_handler)
+    application.add_handler(audio_handler)
 
     application.run_polling()
