cd2c5087b0be092dc6b240c9860b58164f50379c
Silas Martin <sm@akamo.de>
2024-03-07 20:26:11 +0800

    feat: added support for video files and messages

--- main.py
+++ main.py
@@ -22,47 +22,54 @@ TOKEN = os.getenv("TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
-        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
+        parse_mode="MarkdownV2",
+        chat_id=update.effective_chat.id,
+        text="""
+Hi there\! ✌️
+            
+I am a transcription bot designed to transcribe any voice message, video message and plain voice or video files you send me\. If you're interested in this project, look it up on [GitHub](https://github.com/silasmartin/telegram-transcription-bot)\.
+            
+I am using [Whisper](https://github.com/openai/whisper) and an awesome [open source Telegram bot package](https://github.com/python-telegram-bot/python-telegram-bot) under the hood\.
+
+I was developed by [Silas Martin](tg://user?id=378380269), and he would love to hear your feedback\! So, if you're encountering any problems, just reach out\. 
+
+⚠️ Disclaimer\: Although I always keep your and my own privacy in mind while developing software, I offer this experimental service for free to anybody who would like to use it, but I do not guarantee anything, including your privacy\. 
+Use this bot at your own risk\!""",
     )
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
-        chat_id=update.effective_chat.id, text=update.message.text
+        parse_mode="MarkdownV2",
+        chat_id=update.effective_chat.id,
+        text=" Don't send me text \- I'll never know how to react to this\! 😬 Try voice or video messages instead\.",
     )
 
 
 async def transcribe(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print("Received voice message")
+    print("Received message")
+    print(update)
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="Received voice message, transcription running...",
+        text="Received message, transcription running...",
     )
 
     # Get the voice message
-    voice_message = update.message.voice or update.message.audio
+    message_to_transcript = (
+        update.message.voice
+        or update.message.audio
+        or update.message.video_note
+        or update.message.video
+    )
 
-    # Determine the file extension --> adds support for forwarding from WhatsApp
-    file_extension = ".ogg"  # Default to .ogg for voice messages
-    if update.message.voice:
-        file_extension = ".ogg"
-    elif update.message.audio:
-        if update.message.audio.mime_type:
-            mime_type = update.message.audio.mime_type
-            if mime_type == "audio/mp4":
-                file_extension = ".m4a"
-            elif mime_type == "audio/mpeg":
-                file_extension = ".mp3"
-
-    file_path = f"./voice{file_extension}"
+    file_path = "./voice"
 
     # Download the file
-    new_file = await context.bot.get_file(voice_message.file_id)
+    new_file = await context.bot.get_file(message_to_transcript.file_id)
     await new_file.download_to_drive(custom_path=file_path)
 
     # Transcribe the audio file
     result = model.transcribe(file_path)
-    # print(result["text"]) --> Privacy first
     await context.bot.send_message(
         chat_id=update.effective_chat.id, text=result["text"]
     )
@@ -71,13 +78,14 @@ async def transcribe(update: Update, context: ContextTypes.DEFAULT_TYPE):
         print(f"Successfully deleted file: {file_path}")
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
-            text="Successfully deleted voice message from server!",
+            text="Successfully deleted message from server!",
         )
     except OSError as e:
         print(f"Error: {file_path} : {e.strerror}")
         await context.bot.send_message(
+            parse_mode="MarkdownV2",
             chat_id=update.effective_chat.id,
-            text="Error while deleting the voice message from server - your file is still there! Contact the admin.",
+            text="Error while deleting the message from server \- your file is still there! Contact the [admin](tg://user?id=378380269)\.",
         )
 
 
@@ -88,10 +96,14 @@ if __name__ == "__main__":
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     transcription_handler = MessageHandler(filters.VOICE, transcribe)
     audio_handler = MessageHandler(filters.AUDIO, transcribe)
+    video_handler = MessageHandler(filters.VIDEO, transcribe)
+    video_note_handler = MessageHandler(filters.VIDEO_NOTE, transcribe)
 
     application.add_handler(start_handler)
     application.add_handler(echo_handler)
     application.add_handler(transcription_handler)
     application.add_handler(audio_handler)
+    application.add_handler(video_handler)
+    application.add_handler(video_note_handler)
 
     application.run_polling()
